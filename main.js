a3a030422ddc115f44e405c0c06e6ce76632fffe
AngeCruzA <ange.cruz.ag@gmail.com>
2026-02-16 17:28:22 -0600

    Eliminé la condición de main.js

--- main.js
+++ main.js
@@ -2,8 +2,7 @@
 let celsius = Number (prompt ("Ingresa grados Celsius: "));
 //console.log (typeof celsius);
 
-if (typeof celsius === Number){
-    console.log ("Entrada: " +celsius+ " grados Celsius")
+console.log ("Entrada: " +celsius+ " grados Celsius")
 
 let kelvin = celsius + 273.15;
 let fahrenheit = (celsius * 1.8) + 32; 
@@ -11,9 +10,6 @@ let fahrenheit = (celsius * 1.8) + 32;
 console.log ("Conversión de grados");
 console.log ("Salida grados Kelvin: " +kelvin);
 console.log ("Salida grados Fahrenheit: " +fahrenheit);
-} 
-// else {
-    // throw new Error ('Entrada incorrecta. Ingrese un número:');    
-//}
+
 
 
